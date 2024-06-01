41ef428d2ee2fccee4753864d591084080e4d26e
0mChouhan <126401033+0mChouhan@users.noreply.github.com>
2024-06-01 12:34:07 +0530

    Update settings.py

--- ecomprj/settings.py
+++ ecomprj/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%o9!c3rf02q6usr!vw^s96^t*(dsv&ezbs)_u_k7^z1oa$ik0r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://development-server.up.railway.app']
